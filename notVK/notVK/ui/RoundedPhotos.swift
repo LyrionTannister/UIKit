661d03ee120e08fddedadea8141526cfa1fa21d8
Roman Nikishin <loudpiper@gmail.com>
2020-04-04 05:40:02 +0300

    Added round rounded mask for Group Photo

---
+++ notVK/notVK/ui/RoundedPhotos.swift
@@ -0,0 +1,19 @@
+//
+//  RoundedPhotos.swift
+//  notVK
+//
+//  Created by Roman on 04.04.2020.
+//  Copyright © 2020 DrewMyScreen. All rights reserved.
+//
+
+import UIKit
+
+class RoundedPhotos: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
