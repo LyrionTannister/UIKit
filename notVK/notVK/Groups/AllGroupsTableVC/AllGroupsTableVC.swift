b6a7e4ea82f30d74819fae0fcc55f01a2c440f17
Roman Nikishin <loudpiper@gmail.com>
2020-04-12 18:21:14 +0300

    Groups logic rewritten
    <> Changed Group class
    <> Changed logic in allGroups and Groups according the new Group class

--- notVK/notVK/Groups/AllGroupsTableVC/AllGroupsTableVC.swift
+++ notVK/notVK/Groups/AllGroupsTableVC/AllGroupsTableVC.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class AllGroupsTableViewController: UITableViewController {
+    let allGroups: [Group] = [
+        Group(name: "E-squire", count: 100000,
+              groupType: Group.GroupType.openGroup, fotoPath: "iconGroupVK"),
+        Group(name: "E-squire", count: 100000, groupType: Group.GroupType.openGroup, fotoPath: "iconGroupVK"),
+        Group(name: "Snob", count: 30000, groupType: Group.GroupType.openGroup, fotoPath: "iconGroupVK"),
+        Group(name: "Фобошная и Кофешная", count: 25000, groupType: Group.GroupType.openGroup, fotoPath: "iconGroupVK")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    var allGroups = Group.init(groups:
-        ["iOS",
-        "Swift",
-        "GeekBrains",
-        "Mail,ru",
-        "Tool",
-        "iPhone",
-        "Podcasts",
-        "TV Series",
-        "Movies",
-        "Radiohead"])
 
     // MARK: - Table view data source
 
@@ -33,14 +28,14 @@ class AllGroupsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allGroups.myGroups.count
+        return allGroups.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AllGroupsCell", for: indexPath) as! AllGroupsTableViewCell
 
-        let group = allGroups.myGroups[indexPath.row]
-        cell.allGroupsLabel.text = group
+        let group = allGroups[indexPath.row]
+        cell.allGroupsLabel.text = group.name
         return cell
     }
 
