def2e026c742b2584afaace046414e044ce0943a
LyrionTannister <loudpiper@gmail.com>
2020-05-18 16:41:05 +0300

    Added photos loading to Groups

--- notVK/notVK/Sources/Controller/Groups/GroupsTableVC/GroupsTableVC.swift
+++ notVK/notVK/Sources/Controller/Groups/GroupsTableVC/GroupsTableVC.swift
@@ -41,6 +41,9 @@ class GroupsTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "GroupsCell", for: indexPath) as! GroupsViewCell
         //cell.imageRoundedShadowed.image.image = UIImage(named: group.name)
         cell.myGroupLabel.text = groupResponse?.response.items[indexPath.row].name ?? ""
+        if let photoURL = URL(string: (groupResponse?.response.items[indexPath.row].photo_50)!) {
+            cell.imageRoundedShadowed.image.image = UIImage(data: try! Data(contentsOf: photoURL as URL))
+        }
         return cell
     }
 
